e3123cefe3373b6071470909f4a6c5ceffa878c0
santokiya <santokiya@gmail.com>
2018-01-26 21:13:02 -0600

    pull

--- public/assets/js/templatemo_script.js
+++ public/assets/js/templatemo_script.js
@@ -1,75 +1,88 @@
 var top_menu_height = 0;
 jQuery(function($) {
-  $(window).on('load', function() {
-    $('.external-link').unbind('click');
-  });
-
-  $(document).ready(function() {
-
-    // load google map
-    // var script = document.createElement('script');
-    // script.type = 'text/javascript';
-    // script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-    //   'callback=initialize';
-    // document.body.appendChild(script);
-
-    top_menu_height = $('.templatemo-top-menu').height();
-    // scroll spy to auto active the nav item
-    $('body').scrollspy({
-      target: '#templatemo-nav-bar',
-      offset: top_menu_height + 10
-    });
-    $('.external-link').unbind('click');
-
-    // scroll to top
-    $('#btn-back-to-top').click(function(e) {
-      e.preventDefault();
-      scrollTo('#templatemo-top');
+    $(window).on('load', function() {
+        $('.external-link').unbind('click');
     });
 
-    // // scroll to specific id when click on menu
-    // $('.templatemo-top-menu .navbar-nav a').click(function(e) {
-    //   e.preventDefault();
-    //   var linkId = $(this).attr('href');
-    //   scrollTo(linkId);
-    //   if ($('.navbar-toggle').is(":visible") == true) {
-    //     $('.navbar-collapse').collapse('toggle');
-    //   }
-    //   $(this).blur();
-    //   return false;
-    // });
-    /*
-    var dt = window.atob('IHwgRGVzaWduOiA8YSByZWw9Im5vZm9sbG93IiBocmVmPSJodHRwOi8vd3d3LnRlbXBsYXRlbW8uY29tL3RtLTM5NS11cmJhbmljIiB0YXJnZXQ9Il9wYXJlbnQiPlVyYmFuaWM8L2E+'); // decode the string
-    var div = document.getElementById('footer-line');
-    div.innerHTML = div.innerHTML + dt;
-    */
-    // to stick navbar on top
-    $('.templatemo-top-menu ').stickUp();
+    $(document).ready(function() {
 
-    // gallery category
-    $('.templatemo-gallery-category a').click(function(e) {
-      e.preventDefault();
-      $(this).parent().children('a').removeClass('active');
-      $(this).addClass('active');
-      var linkClass = $(this).attr('href');
-      $('.gallery').each(function() {
-        if ($(this).is(":visible") == true) {
-          $(this).hide();
-        }
-      });
-      $(linkClass).fadeIn();
+        // load google map
+        // var script = document.createElement('script');
+        // script.type = 'text/javascript';
+        // script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
+        //   'callback=initialize';
+        // document.body.appendChild(script);
+
+        top_menu_height = $('.templatemo-top-menu').height();
+        // scroll spy to auto active the nav item
+        $('body').scrollspy({
+            target: '#templatemo-nav-bar',
+            offset: top_menu_height + 10
+        });
+        $('.external-link').unbind('click');
+
+        // scroll to top
+        $('#btn-back-to-top').click(function(e) {
+            e.preventDefault();
+            scrollTo('#templatemo-top');
+        });
+
+        // // scroll to specific id when click on menu
+        // $('.templatemo-top-menu .navbar-nav a').click(function(e) {
+        //   e.preventDefault();
+        //   var linkId = $(this).attr('href');
+        //   scrollTo(linkId);
+        //   if ($('.navbar-toggle').is(":visible") == true) {
+        //     $('.navbar-collapse').collapse('toggle');
+        //   }
+        //   $(this).blur();
+        //   return false;
+        // });
+        /*
+        var dt = window.atob('IHwgRGVzaWduOiA8YSByZWw9Im5vZm9sbG93IiBocmVmPSJodHRwOi8vd3d3LnRlbXBsYXRlbW8uY29tL3RtLTM5NS11cmJhbmljIiB0YXJnZXQ9Il9wYXJlbnQiPlVyYmFuaWM8L2E+'); // decode the string
+        var div = document.getElementById('footer-line');
+        div.innerHTML = div.innerHTML + dt;
+        */
+        // to stick navbar on top
+        $('.templatemo-top-menu ').stickUp();
+        // gallery category
+        $('.templatemo-gallery-category a').click(function(e) {
+            e.preventDefault();
+            $(this).parent().children('a').removeClass('active');
+            $(this).addClass('active');
+            var linkClass = $(this).attr('href');
+            $('.gallery').each(function() {
+                if ($(this).is(":visible") == true) {
+                    $(this).hide();
+                }
+            });
+            $(linkClass).fadeIn();
+        });
+
+        //gallery light box setup
+        $('a.colorbox').colorbox({
+            rel: function() {
+                return $(this).data('group');
+
+            }
+        });
     });
-
-    //gallery light box setup
-    $('a.colorbox').colorbox({
-      rel: function() {
-        return $(this).data('group');
-
-      }
-    });
-  });
 });
 
+
+function stickyhead() {
+    window.onscroll = function() { myFunction(); };
+    var header = document.getElementById("myHeader");
+    var sticky = header.offsetTop;
+
+    function myFunction() {
+        if (window.pageYOffset >= sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+}
 
 //google maps initialize chunk was here:
 
@@ -77,10 +90,10 @@ jQuery(function($) {
 // scroll animation
 function scrollTo(selectors) {
 
-  if (!$(selectors).size()) return;
-  var selector_top = $(selectors).offset().top - top_menu_height;
-  $('html,body').animate({
-    scrollTop: selector_top
-  }, 'slow');
+    if (!$(selectors).size()) return;
+    var selector_top = $(selectors).offset().top - top_menu_height;
+    $('html,body').animate({
+        scrollTop: selector_top
+    }, 'slow');
 
 }
